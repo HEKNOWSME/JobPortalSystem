c939ade608c37596ad466a2e72c953ba13473614
Claudistack <iranziclaude@hotmail.com>
2025-10-01 10:13:35 +0200

    fix RegisterForm

--- src/form/RegisterForm.java
+++ src/form/RegisterForm.java
@@ -8,7 +8,6 @@ import java.sql.Connection;
 
 public class RegisterForm extends JFrame {
     private final JLabel usernameLabel = new JLabel("Username"); private final JTextField username = new JTextField(20);
-    private final JLabel emailLabel = new JLabel("Email "); private final JTextField email = new JTextField(20);
     private final JLabel passwordLabel = new JLabel("Password "); private final JPasswordField password = new JPasswordField(20);
     private final JButton loginBtn = new JButton("Login");   private final JButton registerButton = new JButton("Register");
     public RegisterForm() {
@@ -30,12 +29,11 @@ public class RegisterForm extends JFrame {
         setVisible(true);
     }
     private void  createUIComponents() {
-        add(emailLabel); add(email); add(passwordLabel); add(password);
+        add(passwordLabel); add(password);
         add(loginBtn); add(registerButton); add(usernameLabel); add(username);
     }
     private void setComponentsSize() {
         usernameLabel.setBounds(10, 30, 100, 30); username.setBounds(120, 30, 260, 30);
-        emailLabel.setBounds(10, 100, 100, 30); email.setBounds(120, 100, 260, 30);
         passwordLabel.setBounds(10, 170, 100, 30); password.setBounds(120, 170, 260, 30);
         registerButton.setBounds(10, 220, 180, 30); loginBtn.setBounds(195, 220, 180, 30);
     }
@@ -44,20 +42,19 @@ public class RegisterForm extends JFrame {
         new LoginForm();
     }
     private void registerUserToLogin()  {
-        if (username.getText().isEmpty() || email.getText().isEmpty() || new String(password.getPassword()).isEmpty()) {
+        if (username.getText().isEmpty() || new String(password.getPassword()).isEmpty()) {
             JOptionPane.showMessageDialog(null, "Please fill all the fields", "Error", JOptionPane.ERROR_MESSAGE);
         } else {
             try (Connection conn = Db.getConnection()) {
-                var checkEmailStatement  = conn.prepareStatement("SELECT email FROM users WHERE email=?");
-                checkEmailStatement.setString(1, email.getText());
-                var emailCheck = checkEmailStatement.executeQuery();
+                var checkUsername  = conn.prepareStatement("SELECT username FROM users WHERE username=?");
+                checkUsername.setString(1, username.getText());
+                var emailCheck = checkUsername.executeQuery();
                 if (emailCheck.next()) {
                     JOptionPane.showMessageDialog(null, "User already exists", "Error", JOptionPane.ERROR_MESSAGE);
                 } else {
-                    var registerStatement  = conn.prepareStatement("INSERT INTO users (username, email,password) VALUES (?,?,?)");
+                    var registerStatement  = conn.prepareStatement("INSERT INTO users (username,password) VALUES (?,?)");
                     registerStatement.setString(1, username.getText());
-                    registerStatement.setString(2, email.getText());
-                    registerStatement.setString(3, new String(password.getPassword()));
+                    registerStatement.setString(2, new String(password.getPassword()));
                     registerStatement.executeUpdate();
                     JOptionPane.showMessageDialog(null, "User registered successfully", "Success", JOptionPane.INFORMATION_MESSAGE);
                     dispose();
